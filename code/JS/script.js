94ce2dc6eb6f8681910be8967d86ed040e9206bb
MiraeNassr <miraenassr@gmail.com>
2025-10-01 00:06:37 +0200

    correct some syntax error.

--- code/JS/script.js
+++ code/JS/script.js
@@ -4,31 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const fetchHouses = async () => {
         try {
-            const response = await fetch('database.php');
+            const response = await fetch('php/database.php');
             
             if(!response.ok){
-                throw new error('HTTP error! status: ${response.status}');
+                throw new Error('HTTP error! status: ${response.status}');
             }
 
             const houses = await response.json();
 
             houses.forEach(house => {
                 const cardHTML = `
-                <div class ="property-card" data-id="$z{house.id}">
+                <div class ="property-card" data-id="${house.id}">
                     <img src="${house.image}" alt="Huis">
-                    <h3 class="property-price">${house.price}</h3>
+                    <h3 class="property-price">
+  ${house.price && !isNaN(house.price) ? `€${Number(house.price).toLocaleString('nl-NL')}` : 'Prijs onbekend'}
+</h3>
                         <p class="property-desc">${house.description}</p>
                         <p class="property-location">${house.location}</p>
                         <button class="meer-info-btn">Meer info</button>
                     </div>
                 `;
-                propertyListContainer.innerHTML =+ cardHTML;
+               propertyListContainer.innerHTML += cardHTML;
             });
 
             attachEventListeners();
             
         }catch (error) {
-            console.error('Error fetching data:' , error);
+          //  console.error('Error fetching data:'+error);
         propertyListContainer.innerHTML = '<p>Excuses, er zijn momenteel geen woningen beschikbaar. Probeer het later opnieuw.</p>';
         }
     };
@@ -39,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const handleCardClick = (event) => {
         if(event.target.classList.contains('meer-info-btn')) {
-            const card = event.target.closest('.property-car');
+            const card = event.target.closest('.property-card');
             const houseId = card.dataset.id;
 
             alert(`U heeft op Woning ID: ${houseId} geklikt. Dit is de volgende stap!`);
